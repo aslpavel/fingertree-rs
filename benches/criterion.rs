48d682795b0dc1eea4e4a9ce1b4a37f8da127e7b
Pavel Aslanov <asl.pavel@gmail.com>
2021-11-25 13:04:16 +0000

    [bench] convert to use benchmark groups

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Bencher, Criterion, Fun};
+use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion, Throughput};
 use fingertrees::measure::Size;
 use fingertrees::{rc, ArcRefs, FingerTree, Measured, RcRefs, Refs};
 use std::collections::HashMap;
@@ -6,38 +6,47 @@ use std::collections::HashMap;
 const KB: usize = 1024;
 const SPLIT_1024: &[usize] = &[211, 384, 557, 730, 903];
 
-fn bench_from(c: &mut Criterion) {
-    c.bench_function_over_inputs(
-        "create from iter",
-        |b, &&size| b.iter(|| (0..size).map(Size).collect::<rc::FingerTree<_>>()),
-        &[KB, 2 * KB, 4 * KB, 16 * KB],
-    );
+fn ft_collect(c: &mut Criterion) {
+    let mut group = c.benchmark_group("collect");
+    for size in [KB, 2 * KB, 4 * KB, 16 * KB] {
+        group.throughput(Throughput::Elements(size as u64));
+        group.bench_with_input(BenchmarkId::new("size", size), &size, |b, size| {
+            b.iter(|| (0..*size).map(Size).collect::<rc::FingerTree<_>>())
+        });
+    }
+    group.finish();
 }
 
-fn bench_split(c: &mut Criterion) {
+fn ft_split(c: &mut Criterion) {
     let ft: rc::FingerTree<_> = (0..1024).map(Size).collect();
-    c.bench_function_over_inputs(
-        "split",
-        move |b, &size| b.iter(|| ft.split(|m| **m > *size)),
-        SPLIT_1024,
-    );
+
+    let mut group = c.benchmark_group("split");
+    for position in SPLIT_1024 {
+        group.bench_with_input(
+            BenchmarkId::new("position", position),
+            position,
+            |b, position| b.iter(|| ft.split(|m| **m > *position)),
+        );
+    }
+    group.finish();
 }
 
-fn bench_concat(c: &mut Criterion) {
+fn ft_concat(c: &mut Criterion) {
     let ft: rc::FingerTree<_> = (0..1024).map(Size).collect();
     let ft_split: HashMap<_, _> = SPLIT_1024
         .iter()
         .map(|size| (size, ft.split(|m| **m > *size)))
         .collect();
 
-    c.bench_function_over_inputs(
-        "concat",
-        move |b, k| {
-            let (ref left, ref right) = ft_split[*k];
-            b.iter(|| left.concat(right))
-        },
-        SPLIT_1024,
-    );
+    let mut group = c.benchmark_group("concat");
+    for (position, (left, right)) in ft_split {
+        group.bench_with_input(
+            BenchmarkId::new("position", position),
+            &(left, right),
+            |b, (left, right)| b.iter(|| left.concat(right)),
+        );
+    }
+    group.finish();
 }
 
 /// Iterator based destructuring FingerTree with `view`
@@ -73,51 +82,53 @@ where
     }
 }
 
-fn bench_iter(c: &mut Criterion) {
-    let ft: rc::FingerTree<_> = (0..65536).map(Size).collect();
-    c.bench_functions(
-        "iterator",
-        vec![
-            Fun::new("stack", |b, ft: &rc::FingerTree<Size<i64>>| {
-                b.iter(|| ft.iter().fold(0, |count, _| count + 1))
-            }),
-            Fun::new("view", |b, ft: &rc::FingerTree<Size<i64>>| {
-                b.iter(|| ViewIter::new(ft).fold(0, |count, _| count + 1))
-            }),
-        ],
-        ft,
-    );
+fn ft_iter(c: &mut Criterion) {
+    let len = 65536;
+    let ft: rc::FingerTree<_> = (0..len).map(Size).collect();
+    let mut group = c.benchmark_group("iterator");
+    group.bench_with_input(BenchmarkId::new("stack", len), &ft, |b, ft| {
+        b.iter(|| ft.iter().count())
+    });
+    group.bench_with_input(BenchmarkId::new("view", len), &ft, |b, ft| {
+        b.iter(|| ViewIter::new(ft).count())
+    });
+    group.finish();
 }
 
-fn bench_arc_vs_rc(c: &mut Criterion) {
-    fn split_concat<R>(b: &mut Bencher, i: &(usize, usize))
+fn ft_arc_vs_rc(c: &mut Criterion) {
+    fn split_concat<R>(b: &mut Bencher, size_split: &(usize, usize))
     where
         R: Refs<Size<usize>>,
     {
-        let (size, split) = i;
+        let (size, split) = size_split;
         let ft: FingerTree<R, _> = (0..*size).map(Size).collect();
         b.iter(|| {
             let (left, right) = ft.split(|m| **m > *split);
             left.concat(&right)
         })
     }
-    c.bench_functions(
-        "split+concat",
-        vec![
-            Fun::new("arc", split_concat::<ArcRefs>),
-            Fun::new("rc", split_concat::<RcRefs>),
-        ],
-        (16384, 10923),
+    let size_split = (16384, 10923);
+    let mut group = c.benchmark_group("split+concat");
+    group.bench_with_input(
+        BenchmarkId::new("arc", size_split.0),
+        &size_split,
+        split_concat::<ArcRefs>,
     );
+    group.bench_with_input(
+        BenchmarkId::new("rc", size_split.0),
+        &size_split,
+        split_concat::<RcRefs>,
+    );
+    group.finish();
 }
 
 criterion_group! {
     benches,
-    bench_arc_vs_rc,
-    bench_concat,
-    bench_from,
-    bench_iter,
-    bench_split,
+    ft_arc_vs_rc,
+    ft_concat,
+    ft_collect,
+    ft_iter,
+    ft_split,
 }
 
 criterion_main!(benches);
